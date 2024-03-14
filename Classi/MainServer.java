70076ebdab2fc2538e78d66a8abed157fb96da0f
GiacomoCont <113360353+GiacomoCont@users.noreply.github.com>
2024-03-14 08:21:10 +0100

    Modifica

---
+++ Classi/MainServer.java
@@ -0,0 +1,12 @@
+package com.mycompany.comunicazione_unicast_tcp;
+
+/**
+ *
+ * @author Giacomo Contini
+ * 14/03/2024
+ */
+public class MainServer {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
